d6f26304b5e555512e7f8d42838a5f80725712e9
saidul <45492830+saidul00@users.noreply.github.com>
2024-03-12 17:58:25 +0530

    Spring initialization

---
+++ src/test/java/com/saidul/BookMyShow/BookMyShowMovieTicketBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saidul.BookMyShow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowMovieTicketBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
